e7d27fa3a4f3f9cc5fdee87055040c3659c6c814
gaisdav <gais.davletkildin@gmail.com>
2023-03-15 22:28:16 +0100

    added ci

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -2,6 +2,6 @@ import { fn } from "../index";
 
 describe("quadratic equation testing", () => {
   it("test success case", () => {
-    expect(fn(1, 2)).toEqual(5);
+    expect(fn(1, 2)).toEqual(3);
   });
 });
