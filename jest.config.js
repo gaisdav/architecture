ff0b9f9d6dfba028eaecb04660b8b906ed950e53
gaisdav <gais.davletkildin@gmail.com>
2023-03-16 15:04:54 +0100

    hw

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  verbose: true,
+  testEnvironment: "node",
+  clearMocks: true,
+  collectCoverage: true,
 };
